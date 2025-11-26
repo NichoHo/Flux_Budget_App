9111f44626b628bcf1595828a2568a9baea5de6b
Ayman <anang.ayman@gmail.com>
2025-11-26 13:04:53 +0700

    Added english to indo localization

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -12,65 +12,69 @@
 </head>
 <body>
     <h1>Flux - {{ __('Dashboard') }}</h1>
-    
+
     <p>
         Language: 
         <a href="{{ route('lang.switch', 'en') }}">English</a> | 
         <a href="{{ route('lang.switch', 'id') }}">Bahasa Indonesia</a>
     </p>
+    
+        
+        <div style="margin-bottom: 20px;">
+            <p>{{ __('Welcome') }}, {{ Auth::user()->name }}</p>
+            
+            <a href="{{ route('transactions.create') }}" class="btn">+ {{ __('Add Transaction') }}</a>
+            
+            <form action="{{ route('logout') }}" method="POST" style="display:inline;">
+                @csrf
+                <button type="submit">{{ __('Logout') }}</button>
+            </form>
+        </div>
 
-    <div style="margin-bottom: 20px;">
-        <p>Welcome, {{ Auth::user()->name }}</p>
-        <a href="{{ route('transactions.create') }}" class="btn">+ Add Transaction</a>
-        <form action="{{ route('logout') }}" method="POST" style="display:inline;">
-            @csrf
-            <button type="submit">Logout</button>
-        </form>
-    </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>{{ __('Date') }}</th>
+                    <th>{{ __('Description') }}</th>
+                    <th>{{ __('Type') }}</th>
+                    <th>{{ __('Amount') }}</th> <th>{{ __('Receipt') }}</th>
+                    <th>{{ __('Action') }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($transactions as $t)
+                <tr>
+                    <td>{{ $t->created_at->format('Y-m-d') }}</td>
+                    <td>{{ $t->description }}</td>
+                    <td>{{ ucfirst($t->type) }}</td>
+                    
+                    <td>
+                        @if(app()->getLocale() == 'id')
+                            Rp {{ number_format($t->amount, 0, ',', '.') }}
+                        @else
+                            $ {{ number_format($t->amount, 2, '.', ',') }}
+                        @endif
+                    </td>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Date</th>
-                <th>Description</th>
-                <th>Type</th>
-                <th>Amount</th>
-                <th>Receipt</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($transactions as $t)
-            <tr>
-                <td>{{ $t->created_at->format('Y-m-d') }}</td>
-                <td>{{ $t->description }}</td>
-                <td>{{ ucfirst($t->type) }}</td>
-                
-                <td>
-                    @if(app()->getLocale() == 'id')
-                        Rp {{ number_format($t->amount, 0, ',', '.') }}
-                    @else
-                        $ {{ number_format($t->amount, 2, '.', ',') }}
-                    @endif
-                </td>
-
-                <td>
-                    @if($t->receipt_image_url)
-                        <a href="{{ asset('storage/' . $t->receipt_image_url) }}" target="_blank">View Receipt</a>
-                    @else
-                        No Receipt
-                    @endif
-                </td>
-                <td>
-                    <form action="{{ route('transactions.destroy', $t->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="danger">Delete</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+                    <td>
+                        @if($t->receipt_image_url)
+                            <a href="{{ asset('storage/' . $t->receipt_image_url) }}" target="_blank">
+                                {{ __('View Receipt') }}
+                            </a>
+                        @else
+                            {{ __('No Receipt') }}
+                        @endif
+                    </td>
+                    <td>
+                        <form action="{{ route('transactions.destroy', $t->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="danger">{{ __('Delete') }}</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
 </body>
 </html>
